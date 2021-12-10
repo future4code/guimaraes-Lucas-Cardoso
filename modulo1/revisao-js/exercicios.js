bfacd829586e2febe54eecd685fc22f4a947b311
Lucas Cardoso <lucashenrique0221@gmail.com>
2021-12-10 17:41:38 -0300

    Exercicio 7 resolvido

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,45 +5,55 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return array.length
+    return array.length
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  return array.reverse()
+    return array.reverse()
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  return array.sort((a, b) => a - b)
+    return array.sort((a, b) => a - b)
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  return array.filter((number) => number % 2 == 0)
+    return array.filter((number) => number % 2 == 0)
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- return array.filter((number) => number % 2 == 0).map((number) => number * number)
+    return array.filter((number) => number % 2 == 0).map((number) => number * number)
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  let maior = array[0]
+    let maior = array[0]
 
-  for (let i = 0; i < array.length; i++) {
-      if (array[i] > maior) {
-          maior = array[i]
-      }
-  }
-  
-  return maior
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > maior) {
+            maior = array[i]
+        }
+    }
+
+    return maior
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+    const maior = num1 > num2 ? num1 : num2
+    const menor = num1 < num2 ? num1 : num2
+    const divisivel = maior % menor === 0
 
+    const obj = {
+        maiorNumero: maior,
+        maiorDivisivelPorMenor: divisivel,
+        diferenca: maior - menor,
+    }
+
+    return obj
 }
 
 // EXERCÍCIO 08
