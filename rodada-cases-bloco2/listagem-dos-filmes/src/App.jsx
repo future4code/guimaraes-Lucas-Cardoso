a0d861b43110b7974cbcbac9985cb9556673d6d0
Lucas <lucascardoso0221@gmail.com>
2022-10-06 23:08:38 -0300

    Commit inicial

---
+++ rodada-cases-bloco2/listagem-dos-filmes/src/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+`
+
+const App = () => {
+    return (
+        <Container>
+            Salve
+        </Container>
+    )
+}
+
+export default App
---
+++ rodada-cases-bloco2/listagem-dos-filmes/src/globalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    input, button {
+        font-family: 'Montserrat', sans-serif;
+    }
+`
+
+export default GlobalStyle
